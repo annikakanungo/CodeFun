9f83bf1c3d5b41a418646923978e17c1680a527c
Annika <annika.kanungo@gmail.com>
2026-09-04 16:41:29 -0400

    Refactor recent activity function to use handler

--- api/recent-activity.js
+++ api/recent-activity.js
@@ -1,9 +1,6 @@
-export default async function (context, req) {
-  return {
-    status: 200,
-    body: [
-      { action: "Completed challenge", time: "2026-09-04" },
-      { action: "Earned XP", time: "2026-09-03" }
-    ]
-  };
+export default async function handler(req, res) {
+  res.status(200).json([
+    { action: "Completed challenge", time: "2026-09-04" },
+    { action: "Earned XP", time: "2026-09-03" }
+  ]);
 }
