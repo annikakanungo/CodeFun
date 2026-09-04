5778bd327573934f328db853dd0156d062de8b1e
Annika <annika.kanungo@gmail.com>
2026-09-04 17:32:18 -0400

    Delete api/recent-activity.js

--- api/recent-activity.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  res.status(200).json([
-    { action: "Completed challenge", time: "2026-09-04" },
-    { action: "Earned XP", time: "2026-09-03" }
-  ]);
-}
